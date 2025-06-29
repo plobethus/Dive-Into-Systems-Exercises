45eba2d277a5357ff2d44e2d3e0d6fe78e5e20bb
plobethus <135266041+plobethus@users.noreply.github.com>
2025-06-29 10:25:35 -0500

    Not finished

---
+++ ChapterOne/ProblemThree.c
@@ -0,0 +1,130 @@
+/* 3.2 What does this code print
+ * int x
+ * x = 10
+ * if (x < 10){
+ * printf("Sharks\n");
+ * } else {
+ * printf("Jets\n");
+ * }
+ *
+ * Jets
+ *
+ * 3.2 What does this code print
+ * int x
+ * x = 10 
+ * if (x < 10) {
+ * printf("Apple");
+ * } else if (x < 20) {
+ * printf("Banana");
+ * } else if (x < 30) {
+ * printf("Cherry");
+ * }else{
+ * printf("Durian");
+ * }
+ *
+ * Banana
+ *
+ * 3.3 Debug Conditionals 
+ * int x
+ * scanf("%d", &x);
+ * if (x % 2 == 0){
+ * printf("x is divisible by 2\n");
+ * } else if (!(x % 3)) {
+ * printf("x is divisible by 3\n");
+ * } else (x % 5 == 0) {
+ * printf("x is divisible by 5\n");
+ *
+ * line 32, else blocks cannot have a conditional attached to them. 
+ *
+ * 3.4 What does this code print
+ *
+ * int x;
+ * scanf("%d", &x);
+ * if (!x) {
+ * printf("Apple\n");
+ * } else if (x > 0) {
+ * printf("Banana\n");
+ * } else if (x < 0) {
+ * printf("Cherry\n");
+ * } else {
+ * printf("Dragonfruit\n");
+ *
+ * !x evaluates !0 to be true, so the statement breaks at apple. If it was if (x) {, it would be false. 
+ * 
+ * 3.5 What will correctly print out The weather looks good today, if and only if the temperature is in the range 60 to 80 or the humidity is in the range 30 to 50
+ * 
+ * int temp, humidity
+ * scanf("%d", &temp);
+ * scanf("%d", &humidity);
+ *
+ * ((humidity >= 30)&&(humidity <= 50)) || ((temp >= 60)&&(temp <= 80))
+ * ((temp >= 60)&&(temp <= 80)) || (humidity >= 30)&&(humidity<= 50))
+ *int x;
+ * 
+ * 3.6 How many times does this loop print "Hello"?
+ * int i;
+ *
+ * for (i = 1; i < 5; i++) {
+ *      printf("Hello\n");
+ *}
+ * 
+ * 4
+ *
+ * 3.7 How many times does this loop print "Hello"?
+ *
+ * int i;
+ * int x;
+ *
+ * x = 7
+ * for (i = 0; i < x; i++) {
+ *      printf("Hello\n");
+ *      }
+ *
+ *  7 times 
+ *
+ *  3.8 How man times does this loop print "Hello"? 
+ *
+ *  int i;
+ *  for (i = 0; i < -10; i++) {
+ *      printf("Hello\n");
+ *      }
+ *
+ *  3.9 How many times does this loop print "Hello"?
+ *  int i;
+ *  for (i = ; i < 9; i+=2) {
+ *      printf("Hello\n");
+ *      }
+ *
+ *  4
+ *
+ *  3.10 How many times does this loop print "Hello"?
+ *
+ *  int i;
+ *  for (i = 5; i >= 0; i--) {
+ *      printf("Hello\n");
+ *      }
+ *
+ *  6
+ *
+ *  3.111 How many times does this loop print "tweet"?
+ *
+ *  int i; 
+ *  i = 3;
+ *  while (i > 0) {
+ *      printf("tweet\n");
+ *      i--;
+ *      }
+ *
+ *  3
+ * Write a loop that will print out the numbes from 100 to 200 counting by 5s */
+
+#include <stdio.h>
+
+int main(){
+        int i = 100;
+
+        for (i = 100; i < 205; i += 5){
+                printf("%d\n", i);
+                }
+return 0;
+}
